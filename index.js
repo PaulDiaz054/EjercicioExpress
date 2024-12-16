5a1f40d412992d941129ec759ed36bef9d191ab7
PaulDiaz054 <pd1004405054@gmail.com>
2024-12-16 10:49:21 -0500

    Creacion del Web service

--- index.js
+++ index.js
@@ -17,6 +17,24 @@ app.get('/nombre', (req, res) => {
     res.json(lista);
 });
 
+app.get('/Paul', (req, res) => {
+    res.send('Mi nombre es Paul, tengo 20 años y entre mis hobbies favoritos estan los videojuegos y la natacion');
+});
+
+app.get('/suma', (req, res) => {
+    let num1 = 2;
+    let num2 = 1;
+    let suma = num1 + num2;
+    res.send(suma+'');
+});
+
+app.get('/suma/:n1', (req, res) => {
+    let num1 = parseInt(req.params.n1);
+    let num2 = 3;
+    let sum = num1 + num2;
+    res.send('el resultado de la suma es: '+sum);
+});
+
 app.listen(3000, ()=> {
     console.log("Servicio corriendo...");
 });
