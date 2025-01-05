5d8847d365deb1d1e5a2c4f0de22a4c2c42b748e
PaulDiaz054 <pd1004405054@gmail.com>
2025-01-05 16:37:29 -0500

    Creacion del Proyecto para los metodos REST con Express

--- index.js
+++ index.js
@@ -65,9 +65,77 @@ app.get("/rombo/:dmenor&:dmayor&:lado", (req, res) => {
 app.get("/trinomio/:a&:b", (req, res) => {
   let a = parseInt(req.params.a);
   let b = parseInt(req.params.b);
-  res.send("el resultado es: (" + Math.sqrt(a) + "+" + Math.sqrt(b)+ ")^2");
+
+  if(!Number.isInteger(Math.sqrt(a)) && !Number.isInteger(Math.sqrt(b))){
+    res.send("no es un trinomio cuadrado perfecto");
+    return;
+  }
+  res.send("la expresion correcta es:" + a+ "x^2+" + (2*Math.sqrt(a)*Math.sqrt(b)) + "x+" + b +", el resultado es: (" + Math.sqrt(a) + "x+" + Math.sqrt(b)+ ")^2");
 });
 
+app.get("/adiccion", (req, res) => {
+  let n1 = parseInt(req.query.num1);
+  let n2 = parseInt(req.query.num2);
+  let suma = n1 + n2;
+  res.json({resultado: suma});
+});
+
+app.get("/resta", (req, res) => {
+  let parametros = req.query;
+  let n1 = parseInt(parametros.n1);
+  let n2 = parseInt(parametros.n2);
+  let resta;
+  if (n1>n2){
+    resta = n1 - n2;
+  } else {
+    resta = n2 - n1;
+  }
+  res.send(resta + "");
+});
+//calcular un servicio web mediante querys que nos permita conocer el valor a pagar en matriculacion vehicular seguiendo las pautas: 
+// 1. el año del vehiculo: si año > 2000 pagara un subsicion de 2% del valor del vehiculo, si año < 2000 pagara el 5%
+// 2. el valor de matriculacion esta figado en 96$
+// 3. si el vehiculo posee una placa de imbabura, carchi, esmeraldas o sucumbios = descuento del 2.5%
+// 4. si el vehiculo posee multas por cada una pagara 18$
+// indicar los valores calculados en una estructura json y que indique el valor total a pagar
+app.get("/matriculacion", (req, res) => {
+  let valorV = parseInt(req.query.valorV);
+  let anio = parseInt(req.query.anio);
+  let matricula = req.query.matricula;
+  let multas = parseInt(req.query.multas);
+  let valorMatricula = 96;
+  let subsidio;
+  let descuento;
+  let valorMultas;
+  //calcular subsidio por contaminacion
+  if (anio>2000){
+    subsidio = (valorV * 0.02);
+  } else {
+    subsidio = (valorV * 0.05);
+  }
+  //calcular descuento por provincia
+  if (matricula.charAt(0) == "i" || matricula.charAt(0) == "c" || matricula.charAt(0) == "e" || matricula.charAt(0) == "s"){
+    descuento = valorMatricula * 0.025; 
+  } else {
+    descuento = 0;
+  }
+  //calcular multas
+  valorMultas = multas * 18;
+
+  let total = valorMatricula + subsidio - descuento + valorMultas;
+
+  res.json({
+    valorMatricula: valorMatricula,
+    ValorVehiculo: valorV,
+    anio: anio,
+    subsidioContaminacion: subsidio,
+    Matricula: matricula,
+    descuento: descuento,
+    NroMultas: multas,
+    ValorPorMultas: valorMultas,
+    ValorPagar: total
+  });
+});
 app.listen(3000, () => {
   console.log("Servicio corriendo...");
 });
